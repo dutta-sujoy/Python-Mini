2df4873d2e1ebddcb0d33b1cda6fdc8011db196c
SUJOY DUTTA <117010765+dutta-sujoy@users.noreply.github.com>
2023-04-28 22:47:44 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,55 @@
+def add(n1,n2):
+  return n1+n2
+def subtract(n1,n2):
+  return n1-n2
+def multiply(n1,n2):
+  return n1*n2
+def divide(n1,n2):
+  return n1/n2
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+operations = {
+  "+":add,
+  "-":subtract,
+  "*":multiply,
+  "/":divide
+}
+
+def calculator ():
+    print(logo)
+    num1 = float(input("What is the first number? :"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("pick an operation:")
+        num2 = float(input("What is Next number? :"))
+        calculation = operations[operation_symbol]
+        ans = calculation(num1,num2)
+        print(f"{num1} {operation_symbol} {num2} = {ans}")
+
+        if input(f"Type 'y' to continue with {ans} or 'n' to fresh calculation : ") == "y":
+            num1 = ans
+        else:
+            should_continue = False
+        calculator()
+calculator()
+
+  
